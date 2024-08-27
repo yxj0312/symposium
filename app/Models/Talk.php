d3285935d1953ea94dbd0614d303b1364d73cfd2
yxj0312 <yanxiajun@hotmail.com>
2024-08-27 22:36:12 +0200

    add relation method

--- app/Models/Talk.php
+++ app/Models/Talk.php
@@ -10,4 +10,9 @@ class Talk extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function author()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2024_08_25_201808_create_talks_table.php
+++ database/migrations/2024_08_25_201808_create_talks_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->string('type');
             $table->text('abstract');
             $table->text('organizer_notes');
+            $table->foreignIdFor(User::class);
             $table->timestamps();
         });
     }
