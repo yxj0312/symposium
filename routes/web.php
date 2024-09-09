d77746efea3b52c4cd2455ede55c1ef62cc038bb
yxj0312 <yanxiajun@hotmail.com>
2024-09-09 22:02:48 +0200

    add index route

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::get('talks', [TalkController::class, 'index'])->name('talk.index');
     Route::get('talks/create', [TalkController::class, 'create'])->name('talk.create');
     Route::post('talks', [TalkController::class, 'store'])->name('talk.store');
 
