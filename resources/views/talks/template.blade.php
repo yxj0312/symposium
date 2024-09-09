5a28d8515725c08a975bcf4e364d153409546761
yxj0312 <yanxiajun@hotmail.com>
2024-09-09 22:31:58 +0200

    add error input

--- resources/views/talks/template.blade.php
+++ resources/views/talks/template.blade.php
@@ -12,6 +12,7 @@
             <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
               <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>
               <input type="text" name="title" id="title" autocomplete="title" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="janesmith">
+              <x-input-error :message="$errors->get('length')" />
             </div>
           </div>
         </div>
