3370ab70fdba71d37bd2bb5b1f73a62fcf6f4f1f
yxj0312 <yanxiajun@hotmail.com>
2024-09-19 22:33:51 +0200

    update delete page

--- resources/views/components/delete-item.blade.php
+++ resources/views/components/delete-item.blade.php
@@ -1,3 +1,9 @@
 <form method="POST" action="{{ }}">
+  @csrf
 
+  <x-dropdown-link :href="route('logout')"
+    onclick="event.preventDefault();
+        this.closest('form').submit();">
+    {{ __('Log Out') }}
+  </x-dropdown-link>
 </form>
