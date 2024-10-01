9b33743268e908bab786d871d8d4d20bf65621b4
yxj0312 <yanxiajun@hotmail.com>
2024-10-01 22:43:30 +0200

    test

--- resources/views/talks/template.blade.php
+++ resources/views/talks/template.blade.php
@@ -110,7 +110,7 @@
           <label for="length" class="block text-sm font-medium leading-6 text-gray-900">Length</label>
           <div class="mt-2">
             <input type="text" name="length" id="length" autocomplete="length" value = "{{ old('length') }}"class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-            <!-- test 123-->
+            <!-- test 123$-->
           </div>
         </div>
 
