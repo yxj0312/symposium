ad50996edd96072e02cfcab38a92c8896f159108
yxj0312 <yanxiajun@hotmail.com>
2024-10-13 22:02:49 +0200

    remove

--- resources/views/talks/template.blade.php
+++ resources/views/talks/template.blade.php
@@ -110,7 +110,6 @@
           <label for="length" class="block text-sm font-medium leading-6 text-gray-900">Length</label>
           <div class="mt-2">
             <input type="text" name="length" id="length" autocomplete="length" value = "{{ old('length') }}"class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-            <!-- test 123$6/891011121314-->
           </div>
         </div>
 
@@ -195,7 +194,7 @@
       </div>
     </div>
   </div>
-
+  <!-- test -->
   <div class="mt-6 flex items-center justify-end gap-x-6">
     <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
     <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
