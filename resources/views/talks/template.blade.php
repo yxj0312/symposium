a0b54ffc168d005c82f9d899e515b8442d840f43
yxj0312 <yanxiajun@hotmail.com>
2024-10-16 22:07:36 +0200

    test

--- resources/views/talks/template.blade.php
+++ resources/views/talks/template.blade.php
@@ -194,7 +194,7 @@
       </div>
     </div>
   </div>
-  <!-- test 123-->
+  <!-- test 123345-->
   <div class="mt-6 flex items-center justify-end gap-x-6">
     <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
     <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
