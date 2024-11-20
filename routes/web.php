ce7337535eb340e4284419c394147ca78e441db3
yxj0312 <yanxiajun@hotmail.com>
2024-11-20 21:58:41 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::middleware('auth')->group(function () {
     Route::get('talks/{talk}', [TalkController::class, 'show'])->name('talk.show');
     Route::post('talks', [TalkController::class, 'store'])->name('talk.store');
     // test
-    // test2345&678)0111231312123124213
+    // test2345&678)01112313121231242131234567
 });
 
 require __DIR__ . '/auth.php';
